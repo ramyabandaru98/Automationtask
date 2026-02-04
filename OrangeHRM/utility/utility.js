857c902c55e822928d4c08b8c7b03555aa7d3ad9
ramyabandaru98 <89702365+ramyabandaru98@users.noreply.github.com>
2026-02-04 15:42:04 +0530

    error handling

--- OrangeHRM/utility/utility.js
+++ OrangeHRM/utility/utility.js
@@ -1,102 +1,135 @@
 const { remote } = require('webdriverio')
 const { Key } = require('webdriverio')
  
- 
 class Utilities {
  
     static browser = null
     static initializing = null
  
     async getBrowser() {
-        if (Utilities.browser) {
-            return Utilities.browser
-        }
-        if (!Utilities.initializing) {
-            Utilities.initializing = (async () => {
-                Utilities.browser = await remote({
-                    automationProtocol: 'webdriver',
-                    logLevel: 'silent',
-                    capabilities: {
-                        browserName: 'chrome'
-                    }
-                })
+        try {
+            if (Utilities.browser) {
                 return Utilities.browser
-            })()
-        }
+            }
  
-        return Utilities.initializing
+            if (!Utilities.initializing) {
+                Utilities.initializing = (async () => {
+                    Utilities.browser = await remote({
+                        automationProtocol: 'webdriver',
+                        logLevel: 'silent',
+                        capabilities: {
+                            browserName: 'chrome'
+                        }
+                    })
+                    return Utilities.browser
+                })()
+            }
+ 
+            return Utilities.initializing
+        } catch (error) {
+            console.error(error)
+            throw error
+        }
     }
  
     async enterUrl(openurl) {
-        const browser = await this.getBrowser()
-        await browser.url(openurl);
-        await browser.maximizeWindow();
-    }
- 
-    async enterText(selector, value) {
-        const browser = await this.getBrowser()
-        const element = await browser.$(selector)
-        await element.waitForDisplayed()
-        await element.scrollIntoView({ block: 'center' })
-        await this.clearText(selector)
-        await element.click()
-        await element.setValue(value)
-    }
- 
- 
-    async clearText(selector) {
-        const browser = await this.getBrowser();
-        const element = await browser.$(selector);
-        await element.waitForDisplayed();
-        await element.click();
-        await browser.keys(['Control', 'a']);
-        await browser.keys('Delete');
-    }
- 
-    async click(selector) {
-        const browser = await this.getBrowser()
-        const element = await browser.$(selector)
-        await element.waitForDisplayed()
-        await element.scrollIntoView({ block: 'center' })
-        await element.waitForClickable(({
-            timeout: 10000
-        }))
-        await element.click()
-    }
- 
-    async time(value) {
-        const browser = await this.getBrowser();
-        if (value > 0) {
-            await browser.pause(value);
+        try {
+            const browser = await this.getBrowser()
+            await browser.url(openurl)
+            await browser.maximizeWindow()
+        } catch (error) {
+            console.error(error)
+            throw error
         }
     }
  
-        async selectdropdownValue(selector,value) {
-        const browser = await this.getBrowser()
-        const dropdown = await browser.$(`${selector}`)
-        await dropdown.waitForDisplayed()
-        await dropdown.scrollIntoView(({ block: 'center' }))
-        await dropdown.waitForClickable()
-        await dropdown.click()
- 
-        const option = await browser.$(`//div[@role="option" and normalize-space()="${value}"]`)
-        await option.waitForDisplayed()
-        await option.scrollIntoView(({ block: 'center' }))
-        await option.waitForClickable()
-        await option.click()
+    async enterText(selector, value) {
+        try {
+            const browser = await this.getBrowser()
+            const element = await browser.$(selector)
+            await element.waitForDisplayed()
+            await element.scrollIntoView({ block: 'center' })
+            await this.clearText(selector)
+            await element.click()
+            await element.setValue(value)
+        } catch (error) {
+            console.error(error)
+            throw error
+        }
     }
-
  
+    async clearText(selector) {
+        try {
+            const browser = await this.getBrowser()
+            const element = await browser.$(selector)
+            await element.waitForDisplayed()
+            await element.click()
+            await browser.keys(['Control', 'a'])
+            await browser.keys('Delete')
+        } catch (error) {
+            console.error(error)
+            throw error
+        }
+    }
  
+    async click(selector) {
+        try {
+            const browser = await this.getBrowser()
+            const element = await browser.$(selector)
+            await element.waitForDisplayed()
+            await element.scrollIntoView({ block: 'center' })
+            await element.waitForClickable({ timeout: 10000 })
+            await element.click()
+        } catch (error) {
+            console.error(error)
+            throw error
+        }
+    }
+ 
+    async time(value) {
+        try {
+            const browser = await this.getBrowser()
+            if (value > 0) {
+                await browser.pause(value)
+            }
+        } catch (error) {
+            console.error(error)
+            throw error
+        }
+    }
+ 
+    async selectdropdownValue(selector, value) {
+        try {
+            const browser = await this.getBrowser()
+            const dropdown = await browser.$(selector)
+            await dropdown.waitForDisplayed()
+            await dropdown.scrollIntoView({ block: 'center' })
+            await dropdown.waitForClickable()
+            await dropdown.click()
+ 
+            const option = await browser.$(`//div[@role="option" and normalize-space()="${value}"]`)
+            await option.waitForDisplayed()
+            await option.scrollIntoView({ block: 'center' })
+            await option.waitForClickable()
+            await option.click()
+        } catch (error) {
+            console.error(error)
+            throw error
+        }
+    }
  
     async closeBrowser() {
-        if (Utilities.browser) {
-            await Utilities.browser.deleteSession()
-            Utilities.browser = null
-            Utilities.initializing = null
+        try {
+            if (Utilities.browser) {
+                await Utilities.browser.deleteSession()
+                Utilities.browser = null
+                Utilities.initializing = null
+            }
+        } catch (error) {
+            console.error(error)
+            throw error
         }
     }
 }
  
 module.exports = { Utilities }
- 
